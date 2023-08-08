b24d011cb298d36a95355f6289b689561d0f5a0e
Sagar Chauhan <sagarchauhan015@gmail.com>
2023-08-08 23:31:39 +0530

    make the site responsive

--- frontend/weather-app/src/components/Temperature/Temperature.js
+++ frontend/weather-app/src/components/Temperature/Temperature.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import './Temperature.css'
-// import Sunnyday from './assets/sunnyday.png';
 
 const Temperature = (props) => {
+  const dataTime = new Date(props.weatherData.dt).toLocaleTimeString('en-US',{ hour: '2-digit', minute: '2-digit' });
+  const sunriseTime = new Date(props.weatherData.sys.sunrise).toLocaleTimeString('en-US',{ hour: '2-digit', minute: '2-digit' });
+  const sunsetTime = new Date(props.weatherData.sys.sunset).toLocaleTimeString('en-US',{ hour: '2-digit', minute: '2-digit' });
+  
   return (
     <>
         <div className="temp-section">
             <div className="temp-container">
                 <div className="temp-city">
                     <p>{props.weatherData.name}</p>
-                    <p>{props.tempData.temp}</p>
+                    <p>Weather condition : {props.weatherData.weather[0].main}</p>
                 </div>
                 <div className="temp-deg">
                     <div className="temp-deg-left">
-                        <p>{props.tempData.temp}°</p>
+                        <p>{Math.trunc(props.tempData.temp)}°</p>
                         <p><span>Feels Like</span> : {props.tempData.temp}°C</p>
                         <p><span>Low</span> : {props.tempData.temp_min}°C  <span>High</span> : {props.tempData.temp_max}°C</p>
                     </div>
                     <div className="temp-deg-right">
                         <p>Latitude : "{props.weatherData.coord.lat}"</p>
                         <p>Longitude : "{props.weatherData.coord.lon}"</p>
-                        <p>07:15 pm</p>
-                        <p><span>Sunrise </span> : {props.weatherData.sys.lat} <span>Sunset </span> : 42314434</p>
+                        <p>{dataTime}</p>
+                        <p><span>Sunrise </span> : {sunriseTime} <span>Sunset </span> : {sunsetTime}</p>
                     </div>
                 </div>
             </div>
